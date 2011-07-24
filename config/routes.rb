e28349d29718fd9811d7a14c2b2c49ce0c1a3f0a
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-07-24 18:26:38 +0900

    Verifying site for Google OpenID

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Brymck::Application.routes.draw do
   match "about" => "page#about"
-  match "/auth/:provider/callback" => "sessions#create"
+  match "/auth/:provider" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
   root :to => "page#home"
 end
