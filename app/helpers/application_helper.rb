7e7d806ca7073290c92d3465bbb7bbe4afdaeb0d
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-07-27 21:12:51 +0900

    Testing interpolation for i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,7 @@ module ApplicationHelper
 
   def t_meta(name)
     interpolation_data = instance_variable_get("@#{name}_interpolation") || {}
+    raise ["meta.#{controller_path.gsub("/", ".")}.#{action_name}.#{name}", interpolation_data.merge({ :default => t("meta.defaults.#{name}") })].to_yaml
     t("meta.#{controller_path.gsub("/", ".")}.#{action_name}.#{name}",
       interpolation_data.merge({ :default => t("meta.defaults.#{name}") }))
   end
