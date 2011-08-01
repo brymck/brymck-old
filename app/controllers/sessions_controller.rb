fba76f1f59c8beee46d3bb9b849f6b4b42a2c5d9
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-08-02 03:10:12 +0900

    More checking for admin status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < ApplicationController
       'uid'      => auth["uid"]
     )
     determine_admin_status
-    raise session.to_yaml
     redirect_to get_referer, :notice => t(:logged_in, get_locale_and_scope)
   end
 
