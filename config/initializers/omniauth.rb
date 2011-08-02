18c36da88a5423ed507d0b402d30266f3606677f
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-08-03 06:18:51 +0900

    Trying again with LinkedIn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,10 +4,10 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Providers with ID/secret (stored in local environment and Heroku config variables)
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
-  provider :github,   ENV['GITHUB_ID'],   ENV['GITHUB_SECRET']
-  provider :linkedin, ENV['LINKEDIN_ID'], ENV['LINKEDIN_SECRET']
-  provider :twitter,  ENV['TWITTER_ID'],  ENV['TWITTER_SECRET']
+  provider :facebook,  ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  provider :github,    ENV['GITHUB_ID'],   ENV['GITHUB_SECRET']
+  provider :linked_in, ENV['LINKEDIN_ID'], ENV['LINKEDIN_SECRET'], :name => 'linkedin'
+  provider :twitter,   ENV['TWITTER_ID'],  ENV['TWITTER_SECRET']
   
   # Dedicated OpenID
   provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol',    :identifier => 'https://openid.aol.com'
