87f75eae7f2a9a0d8f9268f3617a6c84717a482f
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-08-03 00:31:46 +0900

    Removed exception from login

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   default :to => ENV['EMAIL_ADDRESS']
 
   def login
+    @session, @ip = session, request.remote_ip
     mail :from => "no-reply@brymck.heroku.com"
-    raise session.to_yaml
   end
 end
