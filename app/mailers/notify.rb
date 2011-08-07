ce381fe70addb9edfe76329136de5bfa55ff2cee
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-08-07 19:21:47 +0900

    Testing mailer again

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -1,13 +1,13 @@
 class Notify < ActionMailer::Base
-  default :to => ENV['EMAIL_ADDRESS']
+  default to: ENV['EMAIL_ADDRESS']
 
   def login(auth, session, request)
     @auth, @session, @request = auth, session, request
-    mail :from => "no-reply@brymck.heroku.com"
+    mail from: "no-reply@brymck.heroku.com"
   end
 
   def comment(comment)
     @comment = comment
-    mail :from => "no-reply@brymck.heroku.com"
+    mail from: "no-reply@brymck.heroku.com"
   end
 end
