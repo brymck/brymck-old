b7fba223b1da5a9d27897e27819c5b502c97e2ff
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-08-20 11:40:46 +0900

    Added environments for controlling subscriptions

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -7,6 +7,43 @@ class Subscriber < ActiveRecord::Base
     def address_list
       Subscriber.all.keep_if(&:wants_email?).map(&:header).join(", ")
     end
+
+    def env
+      send_to_all = nil
+
+      Subscriber.all.each do |subscriber|
+        if subscriber.test?
+          return "unknown" if subscriber.inactive?
+        else
+          if send_to_all.nil?
+            send_to_all = subscriber.active?
+          else
+            return "unknown" if subscriber.active? != send_to_all
+          end
+        end
+      end
+
+      case send_to_all
+      when nil then "unknown"
+      when true then "production"
+      when false then "test"
+      end
+    end
+
+    def env=(new_env)
+      case new_env.to_s
+      when "production"
+        send_to_all = true
+      when "test"
+        send_to_all = false
+      else
+        return nil
+      end
+
+      Subscriber.all.each do |subscriber|
+        subscriber.update_attribute :active, subscriber.test? || send_to_all
+      end
+    end
   end
 
   def header
@@ -14,7 +51,7 @@ class Subscriber < ActiveRecord::Base
   end
 
   def wants_email?
-    approved? && active? && !unsubscribed?
+    approved? && active? && subscribed?
   end
 
   def approve!
@@ -27,6 +64,18 @@ class Subscriber < ActiveRecord::Base
 
   def deactivate!
     update_attribute :active, false
+  end
+
+  def unapproved?
+    !approved?
+  end
+
+  def inactive?
+    !active?
+  end
+
+  def subscribed?
+    !unsubscribed?
   end
 
   def status
