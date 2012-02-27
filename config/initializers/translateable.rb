7393d726d5cb84beed139fc61da4ad560444cbb4
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-02-28 08:10:18 +0900

    Added setter method for translateable attributes

--- config/initializers/translateable.rb
+++ config/initializers/translateable.rb
@@ -1,8 +1,13 @@
 module Translateable
   def translateable(*args)
     args.each do |name|
+      # Set up getter method
       define_method name do
-        send("#{name}_#{I18n.locale}") || send("#{name}_en")
+        send "#{name}_#{I18n.locale || "en"}"
+      end
+
+      define_method "#{name}=" do |n|
+        send "#{name}_#{I18n.locale || "en"}=", n
       end
     end
   end
